b644cb69d0731e4a27752ed08ff75f28e5a8cffc
sagardwivedi <sagar.dw20@gmail.com>
2023-06-06 13:05:32 +0530

    Preview card for the right sidebar

---
+++ components/Sidebar/Right/PreviewCard/item.vue
@@ -0,0 +1,14 @@
+<template>
+  <div
+    class="cursor-pointer border-b p-3 hover:bg-gray-100 dark:hover:bg-dim-300"
+    :class="wrapperClasses"
+  >
+    <slot></slot>
+  </div>
+</template>
+<script setup>
+const { defaultTransition, twitterBorderColor } = useTailwindConfig();
+const wrapperClasses = computed(
+  () => `${defaultTransition} ${twitterBorderColor}`
+);
+</script>
