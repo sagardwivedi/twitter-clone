09be2242d7949b14978ec86b995731b54726654c
sagardwivedi <sagar.dw20@gmail.com>
2023-06-06 13:01:10 +0530

    Added configurations

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-
-})
+  typescript: {
+    shim: false,
+  },
+  modules: ["@nuxtjs/tailwindcss"],
+  runtimeConfig: {
+    jwtAccessToken: process.env.JWT_ACCESS_TOKEN,
+    jwtRefreshToken: process.env.JWT_REFRESH_TOKEN,
+  },
+});
