74a287db871722b2859d96d5cdd02675f059547a
sagardwivedi <sagar.dw20@gmail.com>
2023-06-06 13:06:16 +0530

    Left Sidebar

---
+++ components/Sidebar/Left/Tab.vue
@@ -0,0 +1,28 @@
+<template>
+  <NuxtLink
+    to="#"
+    class="flex w-min items-center rounded-full p-3 hover:bg-gray-200 dark:text-white text-black dark:hover:bg-dim-200"
+    :class="defaultTransition"
+  >
+    <div class="h-6 w-6">
+      <slot name="icon"></slot>
+    </div>
+    <div class="ml-4 text-xl hidden xl:block" :class="textClasses">
+      <slot name="name"></slot>
+    </div>
+  </NuxtLink>
+</template>
+<script setup>
+const { defaultTransition } = useTailwindConfig();
+
+const props = defineProps({
+  active: {
+    type: Boolean,
+    default: false,
+  },
+});
+
+const textClasses = computed(() =>
+  props.active ? "font-semibold" : "font-normal"
+);
+</script>
