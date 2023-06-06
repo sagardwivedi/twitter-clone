3b7b1c8f13475452b0eae972f5fc4ac3eb7d065c
sagardwivedi <sagar.dw20@gmail.com>
2023-06-06 13:06:06 +0530

    Right Sidebar

---
+++ components/Sidebar/Right/index.vue
@@ -0,0 +1,71 @@
+<template>
+  <div class="flex flex-col">
+    <!-- Preview Card : What's happening -->
+    <SidebarRightPreviewCard title="What's happening">
+      <SidebarRightPreviewCardItem
+        v-for="whatsHappening in whatsHappeningItems"
+      >
+        <div>
+          <h2 class="text-lg font-bold text-gray-800 dark:text-white">
+            {{ whatsHappening.title }}
+          </h2>
+          <p class="text-xs text-gray-400">{{ whatsHappening.count }}</p>
+        </div>
+      </SidebarRightPreviewCardItem>
+    </SidebarRightPreviewCard>
+
+    <!-- Preview Card : Who to follow -->
+    <SidebarRightPreviewCard title="Who to follow">
+      <SidebarRightPreviewCardItem v-for="whoToFollow in whoToFollowItems">
+        <div class="flex flex-row justify-between items-center p-2">
+          <div class="flex flex-row">
+            <img
+              :src="whoToFollow.image"
+              :alt="whoToFollow.name"
+              class="h-10 w-10 rounded-full"
+            />
+            <div class="ml-2 flex flex-col">
+              <h1 class="text-sm font-bold text-gray-900 dark:text-white">
+                {{ whoToFollow.name }}
+              </h1>
+              <p class="text-xs text-gray-400">{{ whoToFollow.handle }}</p>
+            </div>
+          </div>
+          <div class="flex h-full">
+            <button
+              class="rounded-full bg-black px-4 py-2 text-xs font-bold text-white dark:bg-white dark:text-black"
+            >
+              Follow
+            </button>
+          </div>
+        </div>
+      </SidebarRightPreviewCardItem>
+    </SidebarRightPreviewCard>
+  </div>
+</template>
+
+<script setup>
+const whatsHappeningItems = ref([
+  { title: "SpaceX", count: "18.8k Tweets" },
+  { title: "#CodingISFun", count: "18.8k Tweets" },
+  { title: "#artforall", count: "18.8k Tweets" },
+]);
+
+const whoToFollowItems = ref([
+  {
+    name: "Joe Biden",
+    handle: "@JoeBiden",
+    image: "https://picsum.photos/200/200",
+  },
+  {
+    name: "Joe Biden",
+    handle: "@JoeBiden",
+    image: "https://picsum.photos/200/200",
+  },
+  {
+    name: "Joe Biden",
+    handle: "@JoeBiden",
+    image: "https://picsum.photos/200/200",
+  },
+]);
+</script>
